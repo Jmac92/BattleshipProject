a38eec7d01ec52610b594171ca3ee83d7ce63057
Jmac92 <justin_maciburko@hotmail.com>
2016-05-03 12:26:38 +1000

    Update TestHitTile.cs
    Updated missing explanation of unit test outcomes

--- BattleShipC#working/src/Model/TestHitTile.cs
+++ BattleShipC#working/src/Model/TestHitTile.cs
@@ -13,7 +13,7 @@ namespace MyGame
 			Player testPlayer = new Player (testGame);
 
 			AttackResult actual = testPlayer.PlayerGrid.HitTile (0, 0);
-			Assert.AreEqual ("missed", actual.Text, "yolo");
+			Assert.AreEqual ("missed", actual.Text, "should return 'missed' as there is no ship at the designated tile");
 		}
 
 		[Test]
@@ -25,7 +25,7 @@ namespace MyGame
 			testPlayer.PlayerGrid.MoveShip (0, 0, ShipName.Submarine, Direction.LeftRight);
 
 			AttackResult actual = testPlayer.PlayerGrid.HitTile (0, 0);
-			Assert.AreEqual ("hit something!", actual.Text, "yolo");
+			Assert.AreEqual ("hit something!", actual.Text, "should return 'hit something!' as there is a ship at the designated tile but it should not be destroyed");
 		}
 
 		[Test]
@@ -37,7 +37,7 @@ namespace MyGame
 			testPlayer.PlayerGrid.MoveShip (0, 0, ShipName.Tug, Direction.LeftRight);
 
 			AttackResult actual = testPlayer.PlayerGrid.HitTile (0, 0);
-			Assert.AreEqual ("destroyed the enemy's", actual.Text, "yolo");
+			Assert.AreEqual ("destroyed the enemy's", actual.Text, "should return 'destroyed the enemy's' as there is a ship at the designated tile and it should be destroyed");
 		}
 	}
 }
