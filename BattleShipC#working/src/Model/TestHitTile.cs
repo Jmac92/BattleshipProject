41569bc85419a80692ebc07818735e9d1b3f79dd
jmac92 <justin_maciburko@hotmail.com>
2016-05-03 12:12:02 +1000

    Adds HitTile unit tests file

---
+++ BattleShipC#working/src/Model/TestHitTile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MyGame
+{
+	[TestFixture]
+	public class TestHitTile
+	{
+		[Test]
+		public void TestHitTileNoShip ()
+		{
+			BattleShipsGame testGame = new BattleShipsGame ();
+			Player testPlayer = new Player (testGame);
+
+			AttackResult actual = testPlayer.PlayerGrid.HitTile (0, 0);
+			Assert.AreEqual ("missed", actual.Text, "yolo");
+		}
+
+		[Test]
+		public void TestHitTileHitShip ()
+		{
+			BattleShipsGame testGame = new BattleShipsGame ();
+			Player testPlayer = new Player (testGame);
+
+			testPlayer.PlayerGrid.MoveShip (0, 0, ShipName.Submarine, Direction.LeftRight);
+
+			AttackResult actual = testPlayer.PlayerGrid.HitTile (0, 0);
+			Assert.AreEqual ("hit something!", actual.Text, "yolo");
+		}
+
+		[Test]
+		public void TestHitTileDestroyShip ()
+		{
+			BattleShipsGame testGame = new BattleShipsGame ();
+			Player testPlayer = new Player (testGame);
+
+			testPlayer.PlayerGrid.MoveShip (0, 0, ShipName.Tug, Direction.LeftRight);
+
+			AttackResult actual = testPlayer.PlayerGrid.HitTile (0, 0);
+			Assert.AreEqual ("destroyed the enemy's", actual.Text, "yolo");
+		}
+	}
+}
+
